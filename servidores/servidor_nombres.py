12d7767395bfbc4b390fa0e10c398273e905b486
Germán Barragán G <barragan.gomez.german@gmail.com>
2021-09-21 20:44:28 -0500

    Formato operación reg cambiado, uso de memoria reducido

--- servidores/servidor_nombres.py
+++ servidores/servidor_nombres.py
@@ -1,44 +1,43 @@
 import socket
 import re
 
-directory = {}
-
 def decode(data):
     strData = str(data)
     decoded = {}
     match = re.search(
-        r"[a-z]{3}:([a-z]{3},)?\d+\.\d+(\.\d+\.\d+)?,\d+(\.\d+)?",
+        r"[a-z]{3}:\d+\.\d+,\d+\.\d+|reg:[a-z]{3}",
         strData
     )
     if match:
-        decoded['operation'] = strData[match.start(): strData.index(":")]
-        decoded['data'] = strData[strData.index(":") + 1: match.end()]
+        decoded['operator'] = strData[match.start() : strData.index(":")]
+        decoded['data'] = strData[strData.index(":") + 1 : match.end()]
     return decoded
 
-def call_server(server, data):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    body = bytes(data[0] + "," + data[1], 'utf-8')
+def ask_result_operation(server_address, data, sock):
+    # Prepare the data to be sent
+    body = bytes(data, 'utf-8')
+
     try:
         # Sent data
-        sent = sock.sendto(body, server)
+        sent = sock.sendto(body, server_address)
 
         # Receive response
         data, _ = sock.recvfrom(4096)
-    finally:
-        sock.close()
+    except:
+        data = b"Fallo en la conexion"
     return data
 
-def process(instr, directory):
-    if instr['operation'] == 'reg':
-        unfolded = instr['data'].split(",")
-        directory[unfolded[0]] = tuple(unfolded[1:])
-        result = "ok"
-    elif instr['operation'] in directory:
-        result = call_server(directory[instr['operation']], instr['data'])
+def process(operation, directory, sock, address):
+    if operation['operator'] == 'reg':
+        directory[operation['data']] = address
+        result = b"ok"
+    elif operation['operator'] in directory:
+        result = ask_result_operation(directory[operation['operator']], operation['data'], sock)
     else:
-        result = "Imposible conectar"
-    return str(result)
+        result = b"Operacion no disponible"
+    return result
 
+directory = {}
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -48,6 +47,7 @@ server_address = ('localhost', 20000)
 print('Starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
 
+# Listening to new messages
 while True:
     print('\nListening...')
     data, address = sock.recvfrom(4096)
@@ -56,7 +56,12 @@ while True:
     print(data)
 
     if data:
+        # Decode data
         decoded = decode(data)
-        result = process(decoded, directory)
-        sent = sock.sendto(bytes(result, 'utf-8'), address)
+
+        # Performs the action requested by the client
+        result = process(decoded, directory, sock, address)
+
+        # Send the result back to the client
+        sent = sock.sendto(result, address)
         print('\nSent {} bytes back to {}'.format(sent, address))
