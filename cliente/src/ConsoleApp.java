56b1bec27c3f7de2f9acef81072466365998907f
Germán Barragán G <barragan.gomez.german@gmail.com>
2021-09-23 14:59:20 -0500

    Mejorando presentación de datos

--- cliente/src/ConsoleApp.java
+++ cliente/src/ConsoleApp.java
@@ -10,26 +10,31 @@ public class ConsoleApp {
         Expression expression;
         Client client;
         do {
-            System.out.println("Ingrese la expresión matematica que desea realizar");
-            System.out.println("\n\tPotenciación: a ** b");
+            System.out.println("Ingrese la operacion matematica que desea realizar");
+            System.out.println("\n\tPotenciacion: a ** b");
             System.out.println("\tModulo: a % b");
             System.out.println("\tSuma: a + b");
             System.out.println("\tResta: a - b");
-            System.out.println("\tMultiplicación: a * b");
-            System.out.println("\tDivisión: a / b");
+            System.out.println("\tMultiplicacion: a * b");
+            System.out.println("\tDivision: a / b");
             System.out.println("\nPara salir escriba \"exit\".");
-            System.out.print("Input: ");
+            System.out.print("Operacion: ");
             
             input = reader.nextLine();
+            System.out.println();
             expression = Expression.build(input.replaceAll(" ", ""));
             if(expression != null) {
                 try {
                     client = new Client();
-                    System.out.println(client.sendOperation(expression));
+                    System.out.println(
+                        expression.number1 + " " + expression.operator + " "
+                        + expression.number2 + " = " + client.sendOperation(expression)
+                    );
                 } catch(IOException e) {
                     System.out.println(e.getMessage());
                 }
             }
+            System.out.println();
         } while(!input.equals("exit"));
         reader.close();
     }
